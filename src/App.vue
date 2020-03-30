78cfd45f620b892aae46ca55dc84a9776b344754
olena_rolskaya <manitskayaelena@gmail.com>
2020-03-30 21:35:07 +0300

    buttons upgrade

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,24 @@
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
-          placeholder="Search...." 
-          v-model="query"
-          @keypress="fetchWeather"
-        />
+        <button class="search-bar" v-for="city in cities" :key="city" @click="fetchCity(city)">{{ city }}</button>
+        <button class="search-bar" v-for="city in cities" :key="city" @click="fetchCity(city)">{{ city="London" }}</button>
+        <button class="search-bar" v-for="city in cities" :key="city" @click="fetchCity(city)">{{ city="New York" }}</button>
       </div>
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location"> {{ weather.name}}, {{weather.sys.country}}</div>
-          <div> Atmospheric Pressure: {{weather.main.pressure}} hPa</div>
-          <div> Humidity: {{weather.main.humidity}} %</div>
-          <div> Wind speed: {{weather.wind.speed}} meter/sec</div>
-          <div> Wind direction: {{weather.wind.deg}} °</div>
-          <div class=date>{{ dateBuilder() }}</div>
+          
         </div>
         
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°C </div>
           <div class="weather">{{weather.weather[0].main}}</div>
+          <div class="weather"> Atmospheric Pressure: {{weather.main.pressure}} hPa</div>
+          <div class="weather"> Humidity: {{weather.main.humidity}} %</div>
+          <div class="weather"> Wind speed: {{weather.wind.speed}} meter/sec</div>
+          <div class="weather"> Wind direction: {{weather.wind.deg}} °</div>
         </div>
       </div>
     </main>
@@ -38,31 +34,19 @@ export default {
       api_key: 'a39698134cfd2a34cbe4a458399c6e3d',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
-      weather: {}
+      weather: {},
+      cities: ['Kyiv']
     }
   },
   methods: {
-    fetchWeather (e) {
-      if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+    fetchCity(cityName) {
+      fetch(`${this.url_base}weather?q=${cityName}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
-        }).then(this.setResults);
-      }
-    },
+        }).then(this.setResults) 
+      },
     setResults (results) {
       this.weather = results;
-    },
-    dateBuilder () {
-      let d = new Date();
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-
-      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
