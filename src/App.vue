5ab00dcf0fd2379db0c21ae3a34f2e37b1317cb5
olena_rolskaya <manitskayaelena@gmail.com>
2020-03-30 13:54:36 +0300

    all info loaded

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,16 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location"> {{ weather.name}}, {{weather.sys.country}}</div>
+          <div> Atmospheric Pressure: {{weather.main.pressure}} hPa</div>
+          <div> Humidity: {{weather.main.humidity}} %</div>
+          <div> Wind speed: {{weather.wind.speed}} meter/sec</div>
+          <div> Wind direction: {{weather.wind.deg}} °</div>
           <div class=date>{{ dateBuilder() }}</div>
         </div>
         
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}C</div>
-          <div class="weather">{{ weather.weather[0].main}}</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}°C </div>
+          <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
